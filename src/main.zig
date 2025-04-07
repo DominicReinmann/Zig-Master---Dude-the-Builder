debb63a94f9cdf5ef7f89dc0683e70192dbf7d40
Dominic Reinmann <reinmann.dominic@bluewin.ch>
2025-04-07 20:53:46 +0200

    episod 4

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,9 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const lib = @import("Zig-Master---Dude-the-Builder_lib");
+const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
@@ -32,14 +27,8 @@ test "use other module" {
 test "fuzz example" {
     const global = struct {
         fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
             try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
         }
     };
     try std.testing.fuzz(global.testOne, .{});
 }
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("Zig-Master---Dude-the-Builder_lib");
